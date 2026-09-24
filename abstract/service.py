6776e911977bcfbf5500fa92eb93b94a8cd149be
hagehahahaga <hagehahahaga@qq.com>
2026-09-24 23:28:32 +0800

    fix(service): 服务停止标志更名以恢复 stop() 方法
    __init__ 将实例字段命名为 stop, 与类上的 stop() 方法同名, 实例字段遮蔽了方法, 使 BOT.services[...].stop() 抛出 TypeError: 'bool' object is not callable.
    字段更名为 stopped, 循环判定与停止流程同步更新, stop() 恢复可调用, 停止服务后循环正常退出.
    
    ----------
    
    服务管理指令的 stop 与 restart 恢复正常: 停止指定服务、重启指定服务不再报错, 停止后服务确实不再运行.

--- abstract/service.py
+++ abstract/service.py
@@ -9,7 +9,7 @@ from abstract.bases.custom_thread import CustomThread
 class Service:
     def __init__(self, func: Callable, service_name: str, loop_delay: int | float, auto_restart=False):
         def decorated(*args, **kwargs):
-            while not self.stop:
+            while not self.stopped:
                 try:
                     func(*args, **kwargs)
                 except SendFailure as error:
@@ -23,14 +23,14 @@ class Service:
                     LOG.WAR(f'Service {self} failed.')
                 time.sleep(loop_delay)
             else:
-                self.stop = False
+                self.stopped = False
         self.thread = CustomThread()
         self.args = None
         self.kwargs = None
         self.func = decorated
         self.service_name: str = service_name
         self.auto_restart: bool = auto_restart
-        self.stop = False
+        self.stopped = False
 
     def __str__(self):
         return self.service_name
@@ -49,7 +49,7 @@ class Service:
 
     def stop(self, timeout=None):
         LOG.INF(f'Service {self} stopping...')
-        self.stop = True
+        self.stopped = True
         self.thread.stop(timeout)
         LOG.INF(f'Service {self} stopped.')
 
